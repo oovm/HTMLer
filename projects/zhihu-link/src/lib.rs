9ceff2ac52a0c465db375e5bf09534c3eccd1ce7
Aster <aster@vers.site>
2015-03-15 03:18:25 +0000

    🐣 Parsing the html of Zhihu’s answer

--- projects/zhihu-link/src/lib.rs
+++ projects/zhihu-link/src/lib.rs
@@ -1,3 +1,24 @@
 mod errors;
 
+use std::io::Write;
 pub use errors::{Error, Result};
+use scraper::Html;
+
+#[tokio::test]
+async fn test_reqwest() {
+    let url = "https://www.zhihu.com/question/{}/answer/{}";
+    let client = reqwest::Client::new();
+    let resp = client.get(url).send().await.unwrap();
+    let text = resp.text().await.unwrap();
+    // write to file
+    let mut file = std::fs::File::create("test.html").unwrap();
+    file.write_all(text.as_bytes()).unwrap();
+}
+
+#[test]
+fn test_parse() {
+    let out = Html::parse_document(include_str!("test.html")).unwrap();
+    for node in out.children.iter() {
+        println!("{:#?}", out.children.get(1));
+    }
+}
