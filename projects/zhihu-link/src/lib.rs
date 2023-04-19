4556ac09f9e0aa2b9985700e20aad5adb36bcdf6
Aster <aster@vers.site>
2023-04-19 11:07:28 +0800

    Fetch the Zhihu's article

--- projects/zhihu-link/src/lib.rs
+++ projects/zhihu-link/src/lib.rs
@@ -1,8 +1,10 @@
 #![feature(try_blocks)]
 
 mod answers;
+mod auto;
 mod errors;
+mod utils;
+mod zhuanlans;
 
+pub use crate::{answers::ZhihuAnswer, auto::ZhihuAuto, zhuanlans::ZhihuArticle};
 pub use errors::{ZhihuError, ZhihuResult};
-
-pub use crate::answers::ZhihuAnswer;
--- projects/zhihu-link/tests/main.rs
+++ projects/zhihu-link/tests/main.rs
@@ -1,3 +1,6 @@
+use std::io::Write;
+use zhihu_link::{ZhihuAnswer, ZhihuAuto};
+
 #[test]
 fn ready() {
     println!("it works!")
@@ -15,3 +18,11 @@ fn ready() {
 //     let mut file = std::fs::File::create("test.html").unwrap();
 //     file.write_all(answer.as_bytes()).unwrap();
 // }
+#[tokio::test]
+async fn test_url() {
+    // let answer = ZhihuAuto::new("https://www.zhihu.com/question/30928007/answer/1360071170").unwrap();
+    let answer = ZhihuAuto::new("https://zhuanlan.zhihu.com/p/438085414").await.unwrap();
+    let mut file = std::fs::File::create("test.md").unwrap();
+    file.write_all(answer.as_bytes()).unwrap();
+    // answer.save("test.md").await.unwrap();
+}
